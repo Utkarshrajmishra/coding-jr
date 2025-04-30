11d7e08547ee386ab73d38db7e43584f3fd66a85
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2025-04-30 09:08:48 +0530

    Add: Problem Context

---
+++ context/problemContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react";
+
+type ProblemType = {
+  problemNo: number;
+  runCode: boolean;
+};
+
+type ProblemContextType = {
+  problem: ProblemType;
+  setProblem: React.Dispatch<React.SetStateAction<ProblemType>>;
+};
+
+type ProblemContextProviderProps = {
+  children: React.ReactNode;
+};
+
+export const ProblemContext = createContext<ProblemContextType | null>(null);
+
+export const ProblemContextProvider = ({
+  children,
+}: ProblemContextProviderProps) => {
+  const [problem, setProblem] = useState<ProblemType>({
+    problemNo: 0,
+    runCode: false,
+  });
+
+  return (
+    <ProblemContext.Provider value={{ problem, setProblem }}>
+      {children}
+    </ProblemContext.Provider>
+  );
+};
